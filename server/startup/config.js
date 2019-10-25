fc258e5972e5e1bb41d5b4dd8f0e484d43f1e203
s33dco <code@s33d.co>
2019-10-25 12:07:29 +0100

    troubleshooting deploy

--- server/startup/config.js
+++ server/startup/config.js
@@ -11,6 +11,7 @@ const checkConfig = () => {
 		if (!config.get('backend_port')) {
 			throw new Error('FATAL ERROR: PORT is not defined.');
 		}
+		
 		logger.info(`** successfully read...`);
 		logger.info(`** starting in ${config.util.getEnv('NODE_ENV')}...`);
 	} catch (e) {
