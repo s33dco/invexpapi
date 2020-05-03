8e17c52a901e9947c26a27ee6cb372117c5ef8a1
Ian Marley <ian.marley@talonfms.com>
2020-05-03 01:32:47 +0100

    update node, npm versions

--- client/src/index.js
+++ client/src/index.js
@@ -6,14 +6,15 @@ import MomentUtils from '@date-io/moment';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import App from './components/App';
 import store from './store';
-import 'typeface-roboto'
+import 'typeface-roboto';
 import '../styles/styles.scss';
 
 ReactDOM.render(
-		<Provider store={store}>
-			<MuiPickersUtilsProvider utils={MomentUtils} locale="en">
-				<CssBaseline />
-          <App />
-      </MuiPickersUtilsProvider>
-		</Provider>, document.getElementById('root'));
-
+	<Provider store={store}>
+		<MuiPickersUtilsProvider utils={MomentUtils} locale="en">
+			<CssBaseline />
+			<App />
+		</MuiPickersUtilsProvider>
+	</Provider>,
+	document.getElementById('root')
+);
