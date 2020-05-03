944706712497876f14966de2211ef2fac3c065c7
Ian Marley <ian.marley@talonfms.com>
2020-05-03 23:53:14 +0100

    refix index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import moment from 'moment/moment';
 import MomentUtils from '@date-io/moment';
 import 'moment/locale/en-gb';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -10,11 +9,10 @@ import App from './components/App';
 import store from './store';
 import 'typeface-roboto';
 import '../styles/styles.scss';
-moment.locale('en-gb');
 
 ReactDOM.render(
 	<Provider store={store}>
-		<MuiPickersUtilsProvider utils={MomentUtils} locale="en-gb">
+		<MuiPickersUtilsProvider utils={MomentUtils} locale="en">
 			<CssBaseline />
 			<App />
 		</MuiPickersUtilsProvider>
